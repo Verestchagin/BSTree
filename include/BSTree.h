3a72f3c4e16591c32f48629532d321da47956b6f
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-03-11 11:40:46 +0300

    Update BSTree.h

--- include/BSTree.h
+++ include/BSTree.h
@@ -1,1 +1,16 @@
-
+namespace BSTree{
+	
+	struct Node{
+		int data;
+		Node* left;
+		Node* right;
+	};
+	
+	class Tree{
+		Node* head;
+	    public:
+		Tree();
+		bool input(int value);
+		~Tree();
+	};
+}	
