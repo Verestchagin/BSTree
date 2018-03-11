111b70752377b22f3fef0e378275ee9dab60b2ac
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-03-11 14:04:32 +0300

    Update BSTree.cpp

--- sources/BSTree.cpp
+++ sources/BSTree.cpp
@@ -1,18 +1,51 @@
 #include <iostream>
-#include "BSTree.h"
+#include "bstree.hpp"
+
 using namespace BSTree;
 
-Tree::Tree(){head = nullptr;}
-bool Tree::input(int value){
-	if (head == nullptr) head = new Node{value, nullptr, nullptr};
-	Node *current = head;
-	Node *previous = head;
-	while (current != nullptr){
-		if (current -> data > value)
-			current = current -> left;
-		else current = current -> right;
-		if (current != nullptr) previous = current;	
-	}
-	if (previous -> data > value) parent -> left = new Node{value, nullptr, nullptr};
-	else parent -> right = new Node{value, nullptr, nullptr};
+Tree::Tree(){
+	head = nullptr;
 }
+
+bool Tree::insert(int value) {
+	if (head == nullptr) {
+		head = new Node{value, nullptr, nullptr};
+	}
+
+	Node* current = head;
+	Node* previous = head;
+	while (current != nullptr) {
+		if (current -> data > value) {
+			current = current -> left;
+		}
+		else if (current -> data < value){
+			current = current -> right;
+		}
+		else {
+			return false;
+		}
+		if (current != nullptr) {
+			previous = current;	
+		}
+	}
+
+	if (previous -> data > value) {
+		previous -> left = new Node{value, nullptr, nullptr};
+	}
+	else {
+		previous -> right = new Node{value, nullptr, nullptr};
+	}
+	return true;
+}
+
+void Tree::remove(Node*& node){
+	if (node == nullptr) return;
+	if (node -> left != nullptr) remove(node -> left);
+	if (node -> right != nullptr) remove(node -> right);
+	delete node;
+	node = nullptr;
+}
+Tree::~Tree(){
+	remove(head);
+}
+	
