edbf9c5183ec7a797b8e68a0c6c888cafba1f974
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-03-11 11:39:43 +0300

    Update BSTree.cpp

--- sources/BSTree.cpp
+++ sources/BSTree.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "BSTree.h"
+using namespace BSTree;
 
+Tree::Tree(){head = nullptr;}
+bool Tree::input(int value){
+	if (head == nullptr) head = new Node{value, nullptr, nullptr};
+	Node *current = head;
+	Node *previous = head;
+	while (current != nullptr){
+		if (current -> data > value)
+			current = current -> left;
+		else current = current -> right;
+		if (current != nullptr) previous = current;	
+	}
+	if (previous -> data > value) parent -> left = new Node{value, nullptr, nullptr};
+	else parent -> right = new Node{value, nullptr, nullptr};
+}
+
+Tree::~Tree() { ; }
