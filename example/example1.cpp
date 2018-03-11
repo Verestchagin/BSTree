6e9d93ddb73daaa1d2e1576095eb2bead56271c2
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-03-11 11:40:15 +0300

    Update example1.cpp

--- example/example1.cpp
+++ example/example1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <set>
+#include "BSTree.h"
 using namespace std;
+using namespace BSTree;
 
 int main(int argc, char *argv[]){
 	set<int> unique_set;
@@ -24,8 +26,46 @@ int main(int argc, char *argv[]){
 				unique_set.erase(atoi(argv[i + 1]));
 			}
 		}
-		for (int i = 0; i < n; i++) cout << a[i] << " ";
-		
+		//for (int i = 0; i < n; i++) cout << a[i] << " ";
 	}
-}
 	
+	Tree *tree = new Tree;
+	for (int i = 0; i < n; i++) tree->input(a[i]);
+	
+	string flag_exit = "no";
+	while ((flag_exit != "y") && (flag_exit != "yes") && (flag_exit != "Yes") && (flag_exit != "YES")){
+		cout << endl;
+		cout << "Select one of the operations:" << endl;
+		cout << "1. Print tree" << endl;	
+		cout << "2. Print list of nodes" << endl;
+		cout << "3. Add node" << endl;
+		cout << "4. Delete node" << endl;
+		cout << "5. Save the tree to file" << endl;
+		cout << "6. Load the tree from file" << endl;
+		cout << "7. Check node availability" << endl;
+		cout << "8. End the program" << endl;
+		int ch;
+        	cin >> ch;
+		switch(ch){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				cout << "Do you want to leave program?" << endl;
+				cin >> flag_exit;
+				break;
+			}
+		}
+		cout << "Good bye!" << endl;
+}
