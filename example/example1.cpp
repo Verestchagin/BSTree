47e4d721f4adb30d9fdac70b457ab7b06f5e441f
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-03-11 14:05:36 +0300

    Update example1.cpp

--- example/example1.cpp
+++ example/example1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <set>
-#include "BSTree.h"
+#include "bstree.h"
+
 using namespace std;
 using namespace BSTree;
 
 int main(int argc, char *argv[]){
+	int n;
 	set<int> unique_set;
 	set<int> mini_set;
 	if (argc == 1) cout << "The tree is empty" << endl;
@@ -14,7 +16,7 @@ int main(int argc, char *argv[]){
 			unique_set.insert(atoi(argv[i]));
 			unique_set.insert(atoi(argv[i]) + 1);
 		}
-		int n = int(mini_set.size());
+		n = int(mini_set.size());
 		int a[n];
 		for (int i = 0; i < n; i++) a[i] = 0;
 		//set<int>::iterator ik;
@@ -27,45 +29,43 @@ int main(int argc, char *argv[]){
 			}
 		}
 		//for (int i = 0; i < n; i++) cout << a[i] << " ";
-	}
-	
-	Tree *tree = new Tree;
-	for (int i = 0; i < n; i++) tree->input(a[i]);
-	
-	string flag_exit = "no";
-	while ((flag_exit != "y") && (flag_exit != "yes") && (flag_exit != "Yes") && (flag_exit != "YES")){
-		cout << endl;
-		cout << "Select one of the operations:" << endl;
-		cout << "1. Print tree" << endl;	
-		cout << "2. Print list of nodes" << endl;
-		cout << "3. Add node" << endl;
-		cout << "4. Delete node" << endl;
-		cout << "5. Save the tree to file" << endl;
-		cout << "6. Load the tree from file" << endl;
-		cout << "7. Check node availability" << endl;
-		cout << "8. End the program" << endl;
-		int ch;
-        	cin >> ch;
-		switch(ch){
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 6:
-				break;
-			case 7:
-				break;
-			case 8:
-				cout << "Do you want to leave program?" << endl;
-				cin >> flag_exit;
-				break;
+		Tree tree;
+		for (int i = 0; i < n; i++) tree.insert(a[i]);
+		string flag_exit = "no";
+		while ((flag_exit != "y") && (flag_exit != "yes") && (flag_exit != "Yes") && (flag_exit != "YES")){
+			cout << endl;
+			cout << "Select one of the operations:" << endl;
+			cout << "1. Print tree" << endl;	
+			cout << "2. Print list of nodes" << endl;
+			cout << "3. Add node" << endl;
+			cout << "4. Delete node" << endl;
+			cout << "5. Save the tree to file" << endl;
+			cout << "6. Load the tree from file" << endl;
+			cout << "7. Check node availability" << endl;
+			cout << "8. End the program" << endl;
+			int ch;
+	        	cin >> ch;
+			switch(ch){
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				case 7:
+					break;
+				case 8:
+					cout << "Do you want to leave program?" << endl;
+					cin >> flag_exit;
+					break;
+				}
 			}
-		}
-		cout << "Good bye!" << endl;
+			cout << "Good bye!" << endl;
+	}
 }
