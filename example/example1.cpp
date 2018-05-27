7d7ddebf2142cbd7d1fc890c2837e72325fa7e5f
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-05-27 14:33:07 +0300

    Update example1.cpp

--- example/example1.cpp
+++ example/example1.cpp
@@ -1,88 +1,138 @@
 #include <iostream>
-#include <set>
+#include <vector>
 #include "bstree.hpp"
 
-using namespace std;
 using namespace BSTree;
+using namespace std;
 
-void print_menu(){
-	cout << endl;
-	cout << "Select one of the operations:" << endl;
-	cout << "1. Print tree" << endl;	
-	cout << "2. Print list of nodes" << endl;
-	cout << "3. Add node" << endl;
-	cout << "4. Delete node" << endl;
-	cout << "5. Save the tree to file" << endl;
-	cout << "6. Load the tree from file" << endl;
-	cout << "7. Check node availability" << endl;
-	cout << "8. End the program" << endl;
-}
-
-struct buffer{
-	int* ptr;
-	int size;
-};
-
-buffer parse_cmd(int argc, char *argv[]){
-	int n;
-	set<int> unique_set;
-	set<int> mini_set;
-	for (int i = 1; i < argc; i++){
-		mini_set.insert(atoi(argv[i]));
-		unique_set.insert(atoi(argv[i]));
-		unique_set.insert(atoi(argv[i]) + 1);
-	}
-	n = int(mini_set.size());
-	int *a = new int[n]{0};
-	for (unsigned int i = 0; i < argc - 1; i++){
-		if (unique_set.find(atoi(argv[i + 1])) != unique_set.end()){
-			a[i] = atoi(argv[i + 1]);
-			unique_set.erase(atoi(argv[i + 1]));
+void check_numbers(vector<int>& numbers_array, int argc, char* argv[]) {
+	bool test = false;
+	for (unsigned int i = 1; i < argc; i++) {
+		test = false;
+		for (unsigned int j = 0; j < numbers_array.size(); j++) {
+			if (atoi(argv[i]) == numbers_array[j]) {
+				test = true;
+			}
+		}
+		if (test == false) {
+			numbers_array.push_back(atoi(argv[i]));
 		}
 	}
-	return buffer{a, n};
+}
+void print_menu() {
+	cout << endl;
+	cout << "1: show tree" << endl;
+	cout << "2: show list of tree" << endl;
+	cout << "3: add node in tree" << endl;
+	cout << "4: delete node of tree" << endl;
+	cout << "5: save tree in file" << endl;
+	cout << "6: load tree from file" << endl;
+	cout << "7: search" << endl;
+	cout << "8: finish programm" << endl;
+	cout << endl;
 }
 
-void apply_actions(Tree& tree){
-	string flag_exit = "no";
-	while ((flag_exit != "y") && (flag_exit != "yes") && (flag_exit != "Yes") && (flag_exit != "YES")){
+template <class T>
+void show_list_of_tree(Tree<T>& tree){
+	cout << "a.direct bypass" << endl;
+	cout << "b.symmetric bypass" << endl;
+	cout << "c.back bypass" << endl;
+	char ch;
+	while (true) {
+		cin >> ch;
+			switch (ch) {
+				case 'a':
+					tree.version_bypass(direct);
+					cout << endl;
+					return;
+				case 'b':
+					tree.version_bypass(symmetric);
+					cout << endl;
+					return;
+				case 'c':
+					tree.version_bypass(back);
+					cout << endl;
+					return;
+			}
+	}
+}
+
+bool finish_program() {
+	string choice;
+	while (true) {
+		cout << "Do you want to leave program? ( yes, no )" << endl;
+		cin >> choice;
+		if ((choice == "y") || (choice == "yes") || (choice == "Y") || (choice == "Yes") || (choice == "YES"))
+			return true;
+		else if ((choice == "n") || (choice == "no") || (choice == "N") || (choice == "No") || (choice == "NO"))
+			return false;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	vector<int> numbers_array;
+	Tree<int> tree{5,7,3,9,0,34};
+	cout<<tree<<endl;
+	Tree<int> tree1{tree};
+	cout<<tree1<<endl;
+	Tree<int> tree2;
+	tree2=tree1;
+	cout<<tree2<<endl;
+
+	check_numbers(numbers_array, argc, argv);
+	if (numbers_array.size() > 0) {
+		for(unsigned int i = 0; i < numbers_array.size(); i++)
+			tree.insert(numbers_array[i]);
+	}
+
+	int choice = 0;
+	while (true) {
 		print_menu();
-		int ch;
-        cin >> ch;
-		switch(ch){
+		cin >> choice;
+		int value;
+		string road;
+		switch (choice) {
 			case 1:
+				cout<<tree<<endl;
 				break;
 			case 2:
+				cout<<"e"<<endl;
+				show_list_of_tree(tree);
 				break;
 			case 3:
+			    cout<<"input value for insert"<<endl;
+			    cin>>value;
+			    tree.insert(value);
 				break;
 			case 4:
+			    cout<<"input delete value"<<endl;
+			    cin>>value;
+			    tree.delete_node(value);
 				break;
 			case 5:
+				cout << "please enter filename" << endl;
+				cin>>road;
+			    tree.save_to_file(road);
 				break;
 			case 6:
+				cout << "please enter road to file" << endl;
+				cin>>road;
+			    tree.load_from_file(road);
 				break;
 			case 7:
+				cout << "please enter the item to search for" << endl;
+				cin>>value;
+				tree.exists(value);
 				break;
 			case 8:
-				cout << "Do you want to leave program?" << endl;
-				cin >> flag_exit;
-				break;
+				if (finish_program()){
+					tree.~Tree();
+					return 0;
+				}
+				else
+					continue;
 		}
 	}
-	cout << "Good bye!" << endl;
-}
 
-int main(int argc, char *argv[]){
-	if (argc == 1){
-		 cout << "The tree is empty" << endl;
-		 return 1;
-	}
-	buffer massive = parse_cmd(argc, argv);;
-	Tree tree;
-	for (unsigned int i = 0; i < massive.size; i++){
-		tree.insert(massive.ptr[i]);
-	}
-	apply_actions(tree);
+	return 0;
 }
-	
